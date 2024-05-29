e281e0a3c61dc271f40808120767db55e4ebc907
fan455 <66545913+fan455@users.noreply.github.com>
2024-05-29 11:41:24 +0800

    Delete mathfunc.py

--- mathfunc.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-from scipy.interpolate import CubicSpline, PchipInterpolator
-
-def itp(x, px, py, axis=-1):
-    return CubicSpline(px, py, axis=axis)(x)
-
-def itpmono(x, px, py, axis=-1, extrapolate=False):
-    return PchipInterpolator(px, py, axis=axis, extrapolate=extrapolate)(x)
